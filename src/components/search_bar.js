2c6b88803410c0deb23014ef333c404bf262892a
Sherin Binu <Sherin Binu>
2017-05-05 13:25:04 +0530

    initialized project

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchBar extends Component {
+    render() {
+        return <input type="password" />;
+    }
+}
+
+export default SearchBar;
